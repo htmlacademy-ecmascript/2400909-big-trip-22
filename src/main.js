eb8f08fceef1577b6d691058d5929738c91fc88d
Дмитрий Остапенко <nugashop@yandex.ru>
2023-12-13 14:06:51 +0600

    fix-2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import BoardPresenter from './presenter/trip-list-presenter.js';
-import FilterView from './view/list-filter-view.js';
-import { render } from './render.js';
 
-const siteHeaderElement = document.querySelector('.page-header');
-const filterContainerElement = siteHeaderElement.querySelector('.trip-controls__filters');
-const pageMainElement = document.querySelector('.page-main');
-const pointContainerElement = pageMainElement.querySelector('.trip-events');
+const pointContainerElement = document.querySelector('.trip-events');
+const filterContainerElement = document.querySelector('.trip-controls__filters');
 
-const boardListPresenter = new BoardPresenter({container: pointContainerElement});
-
-render(new FilterView(), filterContainerElement);
+const boardListPresenter = new BoardPresenter(pointContainerElement, filterContainerElement);
 
 boardListPresenter.init();
--- src/presenter/trip-list-presenter.js
+++ src/presenter/trip-list-presenter.js
@@ -1,6 +1,7 @@
+import FilterView from '../view/list-filter-view.js';
 import SortView from '../view/list-sort-view.js';
 import ListView from '../view/list-view.js';
-import AddPointView from '../view/add-point-view.js';
+import PointView from '../view/point-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import {render} from '../render.js';
 
@@ -9,19 +10,20 @@ const POINTS_COUNT = 3;
 export default class BoardPresenter {
   listComponent = new ListView();
 
-  constructor({ container }) {
-    this.container = container;
+  constructor(listContainer, filterContainer) {
+    this.listContainer = listContainer;
+    this.filterContainer = filterContainer;
   }
 
   init() {
-    render(this.listComponent, this.container);
-    render(new SortView(), this.listComponent.getElement());
-    render(new AddPointView(), this.listComponent.getElement());
+    render(new SortView(), this.listContainer);
+    render(new FilterView(), this.filterContainer);
+    render(this.listComponent, this.listContainer);
     render(new EditPointView(), this.listComponent.getElement());
 
 
     for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new ListView(), this.listComponent.getElement());
+      render(new PointView(), this.listComponent.getElement());
     }
   }
 }
