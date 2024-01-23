2ba01ac046acb720b65d2099df711d99617c3656
Дмитрий Остапенко <nugashop@yandex.ru>
2024-01-23 22:21:22 +0600

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import TripPresenter from './presenter/trip-list-presenter.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 //import { generateFilter } from './mock/filter.js';
 //import { generateSort } from './mock/filter.js';
-import { render } from './framework/render.js';
-import FilterView from './view/list-filter-view.js';
+//import { render } from './framework/render.js';
+//import FilterView from './view/list-filter-view.js';
 //import SortView from './view/list-sort-view.js';
-
-const filters = [
-  {
-    type: 'everything',
-  },
-];
 
 const listContainer = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
@@ -20,10 +15,11 @@ const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
 const tripListPresenter = new TripPresenter({listContainer, filterContainer, pointsModel});
 
-render(new FilterView({
-  filters,
-  currentFilterType: 'everything',
-  onFilterChange: () => {}
-}), listContainer);
+const filterPresenter = new FilterPresenter({
+  filterContainer: listContainer,
+  filterModel,
+  pointsModel,
+});
 
+filterPresenter.init();
 tripListPresenter.init();
