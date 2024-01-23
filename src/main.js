53af09815d335ab853829dd456e868912a17ec00
Дмитрий Остапенко <nugashop@yandex.ru>
2024-01-23 21:32:50 +0600

    Добавит модель для фильтра

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import TripPresenter from './presenter/trip-list-presenter.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 //import { generateFilter } from './mock/filter.js';
 //import { generateSort } from './mock/filter.js';
 //import { render } from './framework/render.js';
@@ -10,12 +11,7 @@ const listContainer = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
 
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 const tripListPresenter = new TripPresenter({listContainer, filterContainer, pointsModel});
-
-//const filters = generateFilter(pointsModel.points);
-//const sorts = generateSort(pointsModel.points);
-
-//render(new FilterView({filters}), filterContainer);
-//render(new SortView({sorts}), listContainer);
 
 tripListPresenter.init();
