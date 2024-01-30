a51fe838f0be40fb35577e28c1ea9bc5688dae09
Дмитрий Остапенко <nugashop@yandex.ru>
2024-01-30 22:19:21 +0600

    Добавит модуль TasksApiService в точке входа

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import NewEventButtonView from './view/button-new-event.js';
+import PointsApiService from './points-api-service.js';
+
+const AUTHORIZATION = 'Basic df56gdrg98jnobl';
+const END_POINT = 'https://22.objects.htmlacademy.pro/big-trip';
 
 const listContainer = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
 
-const pointsModel = new PointsModel();
+const pointsModel = new PointsModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
+});
 const filterModel = new FilterModel();
 const tripListPresenter = new TripPresenter({
   listContainer,
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -2,14 +2,27 @@ import { getRandomPoint } from '../mock/points.js';
 import { mockOffers } from '../mock/offers.js';
 import { mockDestinations } from '../mock/destinations.js';
 import Observable from '../framework/observable.js';
-//import { UpdateType, UserAction } from '../const.js';
 
 const POINT_COUNT = 5;
 
 export default class PointsModel extends Observable {
+  #pointsApiService = null;
   #points = Array.from({length: POINT_COUNT}, getRandomPoint);
   #offers = mockOffers;
   #destinations = mockDestinations;
+
+  constructor({pointsApiService}) {
+    super();
+    this.#pointsApiService = pointsApiService;
+
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
 
   get points() {
     return this.#points;
